04c206c424dc0f80126bd790953801d16419fe35
Martin McKee <martinjaymckee@gmail.com>
2021-03-26 11:53:22 -0600

    Added basic structure of the first two Chandra-Rocket files.

---
+++ Chandra-Rocketry/flight_event_detectors.h
@@ -0,0 +1,23 @@
+#ifndef CHANDRA_ROCKETRY_FLIGHT_EVENT_DETECTORS_H
+#define CHANDRA_ROCKETRY_FLIGHT_EVENT_DETECTORS_H
+
+namespace chandra
+{
+namespace rocketry
+{
+template<class Value, uint8_t BufferSize=32>
+class LiftoffDetector
+{
+
+};
+
+template<class Value>
+class BurnoutDetector
+{
+
+};
+
+} /*namespace rocketry*/
+} /*namespace chandra*/
+
+#endif /*CHANDRA_ROCKETRY_FLIGHT_EVENT_DETECTORS_H*/
---
+++ Chandra-Rocketry/rocket_engine_model.h
@@ -0,0 +1,18 @@
+#ifndef CHANDRA_ROCKETRY_ENGINE_MODEL_H
+#define CHANDRA_ROCKETRY_ENGINE_MODEL_H
+
+namespace chandra
+{
+namespace rocketry
+{
+
+template<class Value>
+class RocketEngine
+{
+
+};
+
+} /*namespace rocketry*/
+} /*namespace chandra*/
+
+#endif /*CHANDRA_ROCKETRY_ENGINE_MODEL_H*/
